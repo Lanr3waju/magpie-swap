0bb86212193f4869d7e081fe87affb6fe84417cc
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2025-03-06 14:48:45 +0100

    fix: Isolate swap app tests

--- tests/swap.spec.js
+++ tests/swap.spec.js
@@ -2,24 +2,23 @@ import { test, expect } from '@playwright/test'
 
 test.describe('MagpieFi Swap Page', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('/')
+    await page.goto('https://app.magpiefi.xyz/swap')
     // Handle first-time user modal
     await page.getByRole('button', { name: 'No thanks, skip it' }).click()
-
   })
 
-  test('Navigate to Swap Page and Interact', async ({ page }) => {
+  test('Validate Settings Page on Swap App', async ({ page }) => {
     // Open Settings
     await page.getByRole('button', { name: 'Settings' }).click()
-
     // Verify Settings page
     await expect(page.getByText('Slippage tolerance')).toBeVisible()
     await expect(page.getByText('Liquidity sources')).toBeVisible()
     await expect(page.getByText('Bridges')).toBeVisible()
-
     // Leave Settings page
     await page.locator('div').filter({ hasText: /^Settings$/ }).getByRole('img').click()
+  })
 
+  test('Navigate to Swap Page and Interact', async ({ page }) => {
     // Select token for swapping
     await page.getByRole('button', { name: 'scUSD Sonic 0xd3dc...97ae' }).click()
     await page.getByText('S', { exact: true }).click()
@@ -28,7 +27,6 @@ test.describe('MagpieFi Swap Page', () => {
 
     // Enter amount
     await page.getByPlaceholder('0.00').first().fill('5')
-
     // Verify exchange rate and gas fee
     await page.getByRole('button', { name: '1 =' }).isVisible()
     await page.locator('#gas-fee-value').getByText('$').isVisible()
